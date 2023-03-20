536b15d0e7c6931a17b73f53ce3e3db9be85a607
mmiKky <mm@DESKTOP-2OTU37S>
2023-03-20 12:42:33 +0100

    add asynchronous procedures and stream procedures

--- GrpcClient/src/main/java/GrpcClient.java
+++ GrpcClient/src/main/java/GrpcClient.java
@@ -1,19 +1,22 @@
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import org.example.model.ExampleServiceGrpc;
-import org.example.model.TheRequest;
-import org.example.model.TheResponse;
+import io.grpc.stub.StreamObserver;
+import org.example.model.*;
+
+import java.util.Iterator;
 
 public class GrpcClient {
 
     public static void main(String[] args) {
         String address = "localhost";
         int port = 50001;
-
         ExampleServiceGrpc.ExampleServiceBlockingStub bStub;
+        ExampleServiceGrpc.ExampleServiceStub nonbStub;
+
         System.out.println("Running gRPC client...");
         ManagedChannel channel = ManagedChannelBuilder.forAddress(address, port).usePlaintext().build();
         bStub = ExampleServiceGrpc.newBlockingStub(channel);
+        nonbStub = ExampleServiceGrpc.newStub(channel);
 
         TheRequest request  = TheRequest.newBuilder().setName("Antek").setAge(20).build();
 
@@ -22,6 +25,69 @@ public class GrpcClient {
         System.out.println("...after calling unaryProcedure");
         System.out.println("Response: " + response);
 
+        Iterator<TheResponse> respIterator;
+        System.out.println("...calling streamProcedure");
+        respIterator = bStub.streamProcedure(request);
+        System.out.println("...after calling streamProcedure");
+        TheResponse strResponse;
+        while(respIterator.hasNext()) {
+            strResponse = respIterator.next();
+            System.out.println("streamProcedure response: " + strResponse.getMessage());
+        }
+
+        nonbStub = ExampleServiceGrpc.newStub(channel);
+        System.out.println("...async calling unaryProcedure");
+        nonbStub.unaryProcedure(request, new UnaryObs());
+        System.out.println("...after async calling unaryProcedure");
+
+        System.out.println("...async calling streamProcedure");
+        nonbStub.streamProcedure(request, new UnaryObs());
+        System.out.println("...after async calling streamProcedure");
+
+        OperationReq operationReq = OperationReq.newBuilder().setIsFib(false).setElementsNr(10).build();
+        nonbStub.operationProcedure(operationReq, new OperationObserver());
+
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         channel.shutdown();
+    }
+
+    public static class UnaryObs implements StreamObserver<TheResponse> {
+        @Override
+        public void onNext(TheResponse theResponse) {
+            System.out.println("async unary onNext: " + theResponse.getMessage());
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            System.out.println("async unary onError");
+        }
+
+        @Override
+        public void onCompleted() {
+            System.out.println("async unary onCompleted");
+        }
+    }
+
+    public static class OperationObserver implements StreamObserver<OperationRes> {
+
+        @Override
+        public void onNext(OperationRes operationRes) {
+            System.out.println("async stream onNext: " + operationRes.getResult() + ", " + operationRes.getResultSqr());
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+
+        }
+
+        @Override
+        public void onCompleted() {
+
+        }
     }
 }
--- GrpcServer/src/main/java/GrpcServer.java
+++ GrpcServer/src/main/java/GrpcServer.java
@@ -1,9 +1,7 @@
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-import org.example.model.ExampleServiceGrpc;
-import org.example.model.TheRequest;
-import org.example.model.TheResponse;
+import org.example.model.*;
 
 import java.io.IOException;
 
@@ -22,13 +20,14 @@ public class GrpcServer {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 
     static class ExampleServiceImpl extends ExampleServiceGrpc.ExampleServiceImplBase{
 
         public void unaryProcedure(TheRequest req, StreamObserver<TheResponse> responseObserver) {
             String msg;
-            System.out.println("...called UnaryProcedure");
+            System.out.println("...called UnaryProcedure - start");
             if (req.getAge() > 18)
                 msg = "Mr/Ms " + req.getName();
             else
@@ -36,7 +35,7 @@ public class GrpcServer {
             TheResponse response = TheResponse.newBuilder().setMessage("Hello " + msg).build();
 
             try{
-                Thread.sleep(2000);
+                Thread.sleep(600L);
             } catch (InterruptedException e){
                 e.printStackTrace();
             }
@@ -44,6 +43,52 @@ public class GrpcServer {
             responseObserver.onNext(response);
             responseObserver.onCompleted();
             System.out.println("...called UnaryProcedure - end");
+        }
+
+        @Override
+        public void streamProcedure(TheRequest request, StreamObserver<TheResponse> responseObserver) {
+            int chunksNr = 6;
+
+            System.out.println("...called StreamProcedure - start");
+            for(int i=0; i < chunksNr; ++i){
+                TheResponse response = TheResponse.newBuilder().setMessage("Stream chunk " + (i+1)).build();
+
+                try {
+                    Thread.sleep(300L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                responseObserver.onNext(response);
+            }
+            responseObserver.onCompleted();
+
+            System.out.println("...called StreamProcedure - end");
+        }
+
+        @Override
+        public void operationProcedure(OperationReq request, StreamObserver<OperationRes> responseObserver) {
+
+            if (request.getIsFib()) {
+                int prev = 0, current = 1;
+
+                for (int i = 0; i < request.getElementsNr(); ++i) {
+                    int buff = current;
+                    current += prev;
+                    prev = buff;
+
+                    OperationRes resp = OperationRes.newBuilder().setResult(current).setResultSqr(current * current).build();
+                    responseObserver.onNext(resp);
+                }
+            }
+            else {
+                for(int i = 1, result = 1; i <= request.getElementsNr(); ++i){
+                    result *= i;
+
+                    OperationRes resp = OperationRes.newBuilder().setResult(result).setResultSqr(result * result).build();
+                    responseObserver.onNext(resp);
+                }
+            }
+            responseObserver.onCompleted();
         }
     }
 }
